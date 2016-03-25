47b92d64c0ad49fe1f787ab3357fafd267fdd722
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-03-25 14:07:32 +0100

    save a pool of rolls

--- RollInterface.exs
+++ RollInterface.exs
@@ -1,20 +1,49 @@
 defmodule RollInterface do
 
-  def infinite_loop do
+  def infinite_loop(objs\\%{}) do
+    IO.puts ""
+
     input = String.strip(IO.gets("Input a roll: "))
 
     cond do
       Regex.match? ~r/^e(x(it)?)?$/, input ->
-        :ok
+        objs
+
+      cap = Regex.named_captures(~r/^d(el(ete)?)? (?<name>[A-Za-z]\w*)$/, input) ->
+        objs = Map.delete(objs, cap["name"])
+        IO.puts "***** Deleted #{cap["name"]} *****"
+        infinite_loop(objs)
+
+      input == "" ->
+        IO.puts ""
+        Enum.map(Map.keys(objs), fn(o_name) ->
+          IO.puts "***** #{o_name} *****\t #{Roll.to_string(objs[o_name])} ... #{Roll.test(objs[o_name], true, true)}"
+        end)
+        infinite_loop(objs)
+
       true ->
-        try do
+        cap = Regex.named_captures(~r/^(?<name>[A-Za-z]\w*) +(?<roll>.+)$/, input)
+        add_obj = !is_nil(cap)
+        IO.puts "add_obj:#{add_obj}, name:#{cap["name"]}, roll:#{cap["roll"]}"
+
+        input = cond do
+          add_obj -> cap["roll"]
+          true -> input
+        end
+
+        roll = try do
           roll = Roll.new(input)
           IO.puts "#{Roll.to_string(roll)} ... #{Roll.test(roll, true, true)}"
+          roll
         rescue
           e in RollParserError -> IO.puts e.message
         end
-        IO.puts ""
-        infinite_loop()
+        objs = cond do
+          add_obj -> Map.put(objs, cap["name"], roll)
+          true -> objs
+        end
+
+        infinite_loop(objs)
     end
 
 
